60a6955c586d368138a535f42234039341303782
Abdul Karim Zid <zid.abdulkarim@gmail.com>
2023-01-11 04:38:54 +0100

    Update views.py

--- us/views.py
+++ us/views.py
@@ -14,7 +14,7 @@ def signup(request):
             form.save()
             username = form.cleaned_data['username']
             password = form.cleaned_data['password1']
-            user = user.authenticate(username=username , password=password)
+            user = authenticate(username=username , password=password)
             login(request,user)
             return redirect('profile')
     else:
